c5a22e3e95d9a5120a8d45541c66e9ac4c6af301
Graham Brooks <graham@grahambrooks.com>
2014-07-25 06:40:17 -0400

    Changed to using github API call for the catalog

--- js/controllers.js
+++ js/controllers.js
@@ -5,8 +5,8 @@ var kataApp = angular.module('KatalogApp', ['ngRoute']);
 
 kataApp.controller('KataCtrl', ['$scope', '$routeParams', '$http',
     function ($scope, $routeParams, $http) {
-        $http.get('https://gist.githubusercontent.com/grahambrooks/60f9ce76f3a8f6ed2089/raw/3a152ebf4d55affe7e1870fea9cd5e040fa0f1db/gistfile1.js').success(function (data) {
-            $scope.katalog = data;
+        $http.get('https://api.github.com/repos/grahambrooks/katalog/contents/katalog.json').success(function (data) {
+            $scope.katalog = JSON.parse(atob(data.content.replace(/\s/g, '')));
         });
     }
 ]);
