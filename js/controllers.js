dc838a0f0f708e87e3c6b12b6638cc2d9ddaa305
Graham Brooks <graham@grahambrooks.com>
2014-07-26 12:36:05 -0400

    Added more jekyll support

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,14 @@
-/**
- * Created by graham on 7/24/14.
- */
-var kataApp = angular.module('KatalogApp', ['ngRoute']);
 
-kataApp.controller('KataCtrl', ['$scope', '$routeParams', '$http',
+var CodeKataNinjaApp = angular.module('CodeKataNinjaApp', ['ngRoute']);
+
+CodeKataNinjaApp.config([
+    '$interpolateProvider', function($interpolateProvider) {
+        return $interpolateProvider.startSymbol('{(').endSymbol(')}');
+    }
+]);
+
+
+CodeKataNinjaApp.controller('KatalogController', ['$scope', '$routeParams', '$http',
     function ($scope, $routeParams, $http) {
         $http.get('https://api.github.com/repos/grahambrooks/katalog/contents/katalog.json').success(function (data) {
             $scope.katalog = JSON.parse(atob(data.content.replace(/\s/g, '')));
